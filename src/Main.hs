0189b0720a2072484e6982d92bdb13dba14d42af
Darren Foong <darrenfoong@gmail.com>
2020-03-22 13:27:12 +0800

    Use Lucius

--- src/Main.hs
+++ src/Main.hs
@@ -17,10 +17,15 @@ instance Yesod App
 
 getHomeR = defaultLayout $ do
     setTitle "addki"
+    toWidget [lucius|
+    body { font: 1.0rem/1.1 sans-serif; }
+    #content { padding: 10px; }
+    |]
     [whamlet|<h1>addki|]
     [whamlet|
-    <p><em>addki</em> is a tool to retrieve definitions of foreign words from online dictionaries and convert them into an Anki-importable format.
-    <p>But I still can't use line breaks in my source code without affecting the HTML output...
+    <div #content>
+      <p><em>addki</em> is a tool to retrieve definitions of foreign words from online dictionaries and convert them into an Anki-importable format.
+      <p>But I still can't use line breaks in my source code without affecting the HTML output...
     |]
 
 getJsonR  = return $ object ["message" .= "Hello World"]
