cc14e2560b95ed8dd416698ac4ea895031bc910b
Darren Foong <darrenfoong@gmail.com>
2021-05-29 22:18:29 +0800

    Rename TestServiceApplication

--- src/main/java/addki/AddkiApplication.java
+++ src/main/java/addki/AddkiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestServiceApplication {
+public class AddkiApplication {
   public static void main(String[] args) {
-    SpringApplication.run(TestServiceApplication.class, args);
+    SpringApplication.run(AddkiApplication.class, args);
   }
 }
