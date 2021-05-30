f1c09b339a32ac9158f8d9e03d90f4f6bf62f63f
Darren Foong <darrenfoong@gmail.com>
2021-05-30 19:41:18 +0800

    Tidy

--- src/main/java/addki/controller/EntryController.java
+++ src/main/java/addki/controller/EntryController.java
@@ -3,10 +3,6 @@ package addki.controller;
 import addki.request.CollectEntriesFormRequest;
 import addki.service.EntryService;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -37,13 +33,5 @@ public class EntryController {
     entryService.collectEntries(words, collectEntriesFormRequest.getLanguage());
 
     return "redirect:/";
-  }
-
-  @GetMapping("/logout")
-	public String logout(HttpServletRequest httpServletRequest) {
-	  HttpSession httpServletRequestSession = httpServletRequest.getSession();
-	  httpServletRequestSession.invalidate();
-
-	  return "redirect:/";
   }
 }
